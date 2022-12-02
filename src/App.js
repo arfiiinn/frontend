38155b53b287f48049636a4f77ea67f29340f621
UjualSreedharan <ujualmurikkal@gmail.com>
2022-12-02 12:30:35 +0530

    Verifyuser Form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Login from './Components/Login/Login';
 import Header from './Components/Header/Header'
 import Footer from './Components/Footer/Footer'
+import Verifyuser from './Components/Login/Verifyuser';
 function App() {
   return (
     <div className="App">
-      <Header/>
+      {/* <Header/>
       <Login/>
-      <Footer/>
+      <Footer/> */}
+      <Verifyuser/>
     </div>
   );
 }
